846d4d7be24772f3256d85956831b51bdf916ba7
Sachin Ahuja <sachinahj@gmail.com>
2014-06-22 21:46:07 -0500

    added testing for mergeSort

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,50 +1,49 @@
 require 'pry-byebug'
 
-def sort(array)
+module MergeSort
 
-  return array if array.length == 1
-  half = array.length / 2 - 1
+  def self.sort(array)
+    return array if array.length <= 1
+    half = array.length / 2 - 1
 
-  # puts "array: #{array}"
-  # puts "halfi: #{half}"
-  # puts "half1: #{array[0..half]}"
-  # puts "half2: #{array[half+1..-1]}"
+    # puts "array: #{array}"
+    # puts "halfi: #{half}"
+    # puts "half1: #{array[0..half]}"
+    # puts "half2: #{array[half+1..-1]}"
 
-  # binding.pry
+    # binding.pry
 
-  first = sort(array[0..half]) 
-  second = sort(array[half+1..-1])
-  
-  array = merge(first, second)
-  return array
+    first = sort(array[0..half]) 
+    second = sort(array[half+1..-1])
+    
+    array = MergeSort.merge(first, second)
+    return array
+  end
 
-
-end
-
-def merge(first, second)
-  first_index = 0
-  second_index = 0
-  array = []
-  while first_index < first.length && second_index < second.length 
-    if first[first_index] <= second[second_index]
-      array << first[first_index]
-      first_index += 1
-    else
-      array << second[second_index]
-      second_index += 1
+  def self.merge(first, second)
+    first_index = 0
+    second_index = 0
+    array = []
+    while first_index < first.length && second_index < second.length 
+      if first[first_index] <= second[second_index]
+        array << first[first_index]
+        first_index += 1
+      else
+        array << second[second_index]
+        second_index += 1
+      end
     end
+    if first_index == first.length
+      array += second[second_index...second.length]
+    else
+      array += first[first_index...first.length]
+    end
+    # p array
+    return array
   end
-  if first_index == first.length
-    array += second[second_index...second.length]
-  else
-    array += first[first_index...first.length]
-  end
-  p array
-  return array
-
 
 end
 
-sort([123,23,65,8,3,7,4,8,54,3,6567,89,542,45,7,45,2,34,6,67,83,45,23,446,67,76,3])
+# sort([123,23,65,8,3,7,4,8,54,3,6567,89,542,45,7,45,2,34,6,67,83,45,23,446,67,76,3])
 
 # merge([1,4,5,8],[2,7,8,9])
--- spec/merge_sort_spec.rb
+++ spec/merge_sort_spec.rb
@@ -1,48 +1,48 @@
 require_relative '../merge_sort.rb'
-# require 'rubygems'
+require 'rubygems'
 require 'rspec'
 
-describe 'sort' do
+describe 'MergeSort' do
   describe '.selection_sort' do
 
     context 'when the argument is empty' do
       it "returns an empty array" do
-        expect(Sort.selection_sort([])).to eq([])
+        expect(MergeSort.sort([])).to eq([])
       end
     end
 
     context 'when the argument has negative numbers' do
       it "sorts the array properly" do
         array = [-2, -1, -3, 5, -7 ,-10]
-        expect(Sort.selection_sort(array)).to eq(array.sort)
+        expect(MergeSort.sort(array)).to eq(array.sort)
       end
     end
 
     context 'when the argument has duplicate numbers' do
       it "sorts the array properly" do
         array = [5, 1, 10, 5, 7 ,3, 10]
-        expect(Sort.selection_sort(array)).to eq(array.sort)
+        expect(MergeSort.sort(array)).to eq(array.sort)
       end
     end
 
     context 'when the argument is large unordered array' do
       it "sorts the array properly" do
         array = [123,23,65,8,3,7,4,8,54,3,6567,89,542,45,7,45,2,34,6,67,83,45,23,446,67,76,3]
-        expect(Sort.selection_sort(array)).to eq(array.sort)
+        expect(MergeSort.sort(array)).to eq(array.sort)
       end
     end
 
     context 'when the argument is a size one' do
       it "returns the array with one value" do
         array = [9]
-        expect(Sort.selection_sort(array)).to eq(array.sort)
+        expect(MergeSort.sort(array)).to eq(array.sort)
       end
     end
 
     context 'when the argument melody of negatives, positives and zeros' do
       it "sorts the array properly" do
         array = [3 ,5, -6, 4, -7, 1, 2, 0, -12, 13]
-        expect(Sort.selection_sort(array)).to eq(array.sort)
+        expect(MergeSort.sort(array)).to eq(array.sort)
       end
     end
 
